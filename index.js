85525555a1cc0004f466ddff8aad1540a0bd7586
root <emilianopimbas@gmail.com>
2024-12-05 20:16:24 -0300

    TEST Added js to main page

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+*	Chat Connect, allstays.com.br
+*/
+
+
+function Startup(){
+	let get = document.getElementById("popup");
+	get.animate([
+		{top: 100vh},
+		{top: 80vh}	
+	]);
+}	
+
+
+
+Startup();
